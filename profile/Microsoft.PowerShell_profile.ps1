4e065e57ccc747e0f70dc5eff3e3d82f3d2e4227
bao <131514603+ruru-o@users.noreply.github.com>
2026-08-09 20:29:54 +0800

    Add files via upload

---
+++ profile/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,14 @@
+oh-my-posh init pwsh --config 'agnosterplus' | Invoke-Expression
+
+try {
+    [Console]::InputEncoding  = [System.Text.Encoding]::UTF8
+    [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+    $OutputEncoding = [System.Text.UTF8Encoding]::new($false)
+    chcp 65001 > $null
+} catch {}
+
+Clear-Host
+
+if (Get-Command fastfetch -ErrorAction SilentlyContinue) {
+    fastfetch -c "C:/Users/User/.config/fastfetch/config.jsonc"
+}
